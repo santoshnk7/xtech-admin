620288a6339632d8227e76f2e0f6f29dd4bfd8ee
santoshnk7 <santoshnaik51@gmail.com>
2014-09-20 00:53:43 -0700

    init5

--- config/deploy.rb
+++ config/deploy.rb
@@ -90,11 +90,11 @@ namespace :deploy do
 
   # remove the default nginx configuration as it will tend
   # to conflict with our configs.
-  before 'deploy:setup_config', 'nginx:remove_default_vhost'
+  #before 'deploy:setup_config', 'nginx:remove_default_vhost'
 
   # reload nginx to it will pick up any modified vhosts from
   # setup_config
-  after 'deploy:setup_config', 'nginx:reload'
+  #after 'deploy:setup_config', 'nginx:reload'
 
   # As of Capistrano 3.1, the `deploy:restart` task is not called
   # automatically.
